748166f7fda835982c343d914f897923f8eb76cb
ViridianAStar <bowman9594@gmail.com>
2026-03-23 15:02:02 -0400

    Do some simple trie like stuff

--- Tree_Like/trie_tree_wordlike.cpp
+++ Tree_Like/trie_tree_wordlike.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "trie_tree_wordlike.h"
+
+template<typename content>
+ttw_node<content> trie_tree_wordlike<content>::findchild(ttw_node<content> *node, const content ctnts) {
+
+    for (ttw_node<content> item : *node->children) {
+        if (item.content == ctnts) {
+            return item;
+        }
+    }
+
+    return nullptr;
+}
+
+template<typename content>
+void trie_tree_wordlike<content>::insertStatement(content ctnts[]) {
+    ttw_node<content>* current = root;
+
+    for (content* info : ctnts) {
+        ttw_node<content>* child = findchild(current, info);
+
+        if (child != nullptr) {
+            ttw_node<content>* child = ttw_node<content>(info, current, false);
+            child->set_parent() = current;
+            current->add_child(child);
+            if (loopable_info.contains(info)) {
+                child->set_loopable(true);
+            }
+        }
+
+        current = child;
+    }
+
+    current->set_end(true);
+}
--- Tree_Like/trie_tree_wordlike.h
+++ Tree_Like/trie_tree_wordlike.h
@@ -4,6 +4,7 @@
 
 #ifndef PLAYGROUND_TRIE_TREE_WORDLIKE_H
 #define PLAYGROUND_TRIE_TREE_WORDLIKE_H
+#include <unordered_map>
 #include <vector>
 
 
@@ -14,12 +15,12 @@
 template<typename content>
 class ttw_node {
 private:
-    content contents;
     ttw_node* parent;
     bool loopable = false;
     std::pmr::vector<ttw_node*> children;
     bool end;
 public:
+    content contents;
     /**
      *
      * @param lpbl boolean value to set for loopable
@@ -31,6 +32,9 @@ public:
      * @return
      */
     [[nodiscard]] bool get_loopable() const {return loopable;}
+
+    void set_end(const bool ends) {end = ends;}
+    [[nodiscard]] bool get_end() const {return end;}
 
     /**
      * If for some reason you must move a node to a different part of the structure you can set its parent elsewhere
@@ -60,11 +64,22 @@ public:
      * @param parent
      * @param ends
      */
-    ttw_node(content ctnts, ttw_node* parent, const bool ends) : contents(ctnts), parent(parent), end(ends) {}
+    ttw_node(content ctnts, ttw_node* parent, const bool ends) : parent(parent), end(ends), contents(ctnts) {}
 };
 
-class trie_tree_wordlike {
 
+template<typename content>
+class trie_tree_wordlike {
+private:
+    // value -10 integer by default can contain other types as children
+    ttw_node<int> root = ttw_node<int>(-10, nullptr, false);
+
+    ttw_node<content> findchild(ttw_node<content>* node, const content ctnts);
+
+     std::pmr::unordered_map<content, bool> loopable_info;
+    public:
+        void insertStatement(content ctnts[]);
+        bool isValid(content ctnts[]);
 };
 
 
